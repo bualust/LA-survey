161014388ea2be60f54a50899dbf2a429c7e8f59
Valentina Vecchio <valentina.vecchio@cern.ch>
2021-09-27 23:04:17 +0100

    plotting improvements

--- survey_analysis.py
+++ survey_analysis.py
@@ -43,26 +43,26 @@ def looper(AGE):
     print('--- Number of answers available: ',num_lines-1)
 
     arr_tsv = np.genfromtxt(FILE, delimiter='\t', dtype='object',encoding='utf-8') #cp1252 to be used to decode
-    number_of_questions = len(arr_tsv[0,:]) -1  #number of plots (ghe first one is the date, we can ignore it)
+    number_of_questions = len(arr_tsv[0,:]) -1  #number of plots (the first one is the date, we can ignore it)
     print('--- Number of questions posed: ',number_of_questions)
 
     i=1
-    while i <= number_of_questions-4: 
-       print('Currently processing question number: ',i)
-       fig, ax = plt.subplots(2,2)
-       if i==1:
-          ax[0,0].hist(np.array(arr_tsv[1:,1],dtype=int), [15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100])
+    while i <= number_of_questions: 
+       if i==6 or i==5:
+          print('Skipping question number:  ',i)
+          i=i+1
        else:
-          ax[0,0].hist(arr_tsv[1:,i])
-       ax[0,0].set_title(arr_tsv[0,i].decode('cp1252'))
-       ax[0,1].hist(arr_tsv[1:,i+1])
-       ax[0,1].set_title(arr_tsv[0,i+1].decode('cp1252'))
-       ax[1,0].hist(arr_tsv[1:,i+2])
-       ax[1,0].set_title(arr_tsv[0,i+2].decode('cp1252'))
-       ax[1,1].hist(arr_tsv[1:,i+3])
-       ax[1,1].set_title(arr_tsv[0,i+3].decode('cp1252'))
-       plt.show()
-       i=i+4
+          print('Currently processing question number: ',i)
+          fig, (ax1) = plt.subplots(1)
+          if i==1:
+             ax1.hist(np.array(arr_tsv[1:,i],dtype=int), [15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100])
+          #elif i>=9 and i<=14:
+          #   ax1.hist(np.array(arr_tsv[1:,i]), ["per nulla","poco","abbastanza","molto","non so"])
+          else:
+             ax1.hist(arr_tsv[1:,i])
+          ax1.set_title(arr_tsv[0,i].decode('cp1252'))
+          plt.show()
+          i=i+1
     print('Next bunch of plots')
 
 # __main__
